034044bfe3c939d310ed5ed3d5636c441d98d2c2
danehay <haydane9@gmail.com>
2018-08-18 22:44:10 +0700

    route

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ButtonSwitch from '../components/ButtonSwitch';
+import { BrowserRouter, Router } from 'react-router-dom';
 export default class home extends React.Component
 {
      render()
      {
-          return(
-               <div>
-                    
-               </div>
+          return
+          (
+               <BrowserRouter>
+                    <Router path='/' component={ButtonSwitch}/>
+               </BrowserRouter>     
           );
      }
 }
